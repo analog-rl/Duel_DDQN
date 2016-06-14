b251e21f299e05643928065eb25a1ee493dbbf62
Joe Booth <joe@joebooth.com>
2016-06-14 15:22:55 -0700

    wit trying to get CNN working

--- duel.py
+++ duel.py
@@ -2,36 +2,148 @@ import argparse
 import gym
 from gym.spaces import Box, Discrete
 from keras.models import Model
-from keras.layers import Input, Dense, Lambda
+from keras.layers.core import Dense, Dropout, Activation, Flatten, Reshape
+from keras.layers import Convolution2D, ZeroPadding2D, Flatten, Dense, Activation, Input, Lambda, TimeDistributed
 from keras.layers.normalization import BatchNormalization
 from keras import backend as K
 from keras.optimizers import Adam, RMSprop
 import numpy as np
 
+import os
+print ("$PATH = ", os.environ['PATH'])
 
-def createLayers():
-    x = Input(shape=env.observation_space.shape)
-    if args.batch_norm:
-        h = BatchNormalization()(x)
-    else:
-        h = x
-    for i in range(args.layers):
-        h = Dense(args.hidden_size, activation=args.activation)(h)
-        if args.batch_norm and i != args.layers - 1:
-            h = BatchNormalization()(h)
-    y = Dense(env.action_space.n + 1)(h)
-    if args.advantage == 'avg':
-        z = Lambda(lambda a: K.expand_dims(a[:, 0], dim=-1) + a[:, 1:] - K.mean(a[:, 1:], keepdims=True),
-                   output_shape=(env.action_space.n,))(y)
-    elif args.advantage == 'max':
-        z = Lambda(lambda a: K.expand_dims(a[:, 0], dim=-1) + a[:, 1:] - K.max(a[:, 1:], keepdims=True),
-                   output_shape=(env.action_space.n,))(y)
-    elif args.advantage == 'naive':
-        z = Lambda(lambda a: K.expand_dims(a[:, 0], dim=-1) + a[:, 1:], output_shape=(env.action_space.n,))(y)
-    else:
-        assert False
+# import os
+# # os.environ["THEANO_FLAGS"] = "mode=FAST_RUN,floatX=float32"
+# os.environ["KERAS_BACKEND"] = "tensorflow"
 
-    return x, z
+
+def create_model():
+    input_dim_orig = env.observation_space.shape
+
+    S = Input(shape=input_dim_orig)
+    h = Convolution2D(32, 8, 8, subsample=(4, 4), border_mode='same', activation='relu', init='normal')(S)
+    h = Convolution2D(64, 4, 4, subsample=(2, 2), border_mode='same', activation='relu', init='normal')(h)
+    h = Convolution2D(64, 3, 3, subsample=(1, 1), border_mode='same', activation='relu', init='normal')(h)
+    h = Flatten()(h)
+    #TODO DDQN split here--
+    # Value = Dense 512 -> Dense 1
+    # Advantage = Dense 512 -> Dense n.actions
+    h = Dense(512, activation='relu', init='normal')(h)
+    V = Dense(env.action_space.n, activation='linear', init='normal')(h)
+    model = Model(S, V)
+    return model
+
+#form paper
+# def create_model():
+#     # # input_dim_orig = [2] + list(env.observation_space.shape)
+#     # input_dim_orig = [1] + list(env.observation_space.shape)
+#     # input_dim = np.product(input_dim_orig)
+#     # S = Input(shape=[input_dim])
+#     # h = Reshape(input_dim_orig)(S)
+#     input_dim_orig = env.observation_space.shape
+#
+#     S = Input(shape=input_dim_orig)
+#     h = Convolution2D(32, 8, 8, subsample=(4, 4), border_mode='same', activation='relu')(S)
+#     h = Convolution2D(64, 4, 4, subsample=(2, 2), border_mode='same', activation='relu')(h)
+#     h = Convolution2D(64, 3, 3, subsample=(1, 1), border_mode='same', activation='relu')(h)
+#     h = Flatten()(h)
+#     #TODO DDQN split here--
+#     # Value = Dense 512 -> Dense 1
+#     # Advantage = Dense 512 -> Dense n.actions
+#     h = Dense(512, activation='relu')(h)
+#     V = Dense(env.action_space.n, activation='linear', init='zero')(h)
+#     model = Model(S, V)
+#     return model
+
+# note: i could not get this to work
+def create_kerlym_model():
+    # S = Input(shape=[env.observation_space.shape])
+    # input_dim_orig = [2] + list(env.observation_space.shape)
+    input_dim_orig = [1] + list(env.observation_space.shape)
+    # input_dim_orig = list(env.observation_space.shape)
+    input_dim = np.product(input_dim_orig)
+    S = Input(shape=[input_dim])
+    h = Reshape(input_dim_orig)(S)
+    h = TimeDistributed( Convolution2D(16, 8, 8, subsample=(4, 4), border_mode='same', activation='relu'))(h)
+    h = TimeDistributed( Convolution2D(32, 4, 4, subsample=(2, 2), border_mode='same', activation='relu'))(h)
+    h = Flatten()(h)
+    h = Dense(256, activation='relu')(h)
+    V = Dense(env.action_space.n, activation='linear',init='zero')(h)
+    model = Model(S, V)
+    return model
+
+
+# def create_model():
+#     trainable = True
+#     cnn_model = Sequential()
+#     x = Input(shape=env.observation_space.shape)
+#     if args.batch_norm:
+#         h = BatchNormalization()(x)
+#     else:
+#         h = x
+#     cnn_model.add(ZeroPadding2D(input_shape=env.observation_space.shape))
+#     # cnn_model.add(h)
+#     cnn_model.add(Convolution2D(32, 8, 8, subsample=(4, 4), trainable=trainable, activation=args.activation))
+#     cnn_model.add(Convolution2D(64, 4, 4, subsample=(2, 2), trainable=trainable, activation=args.activation))
+#     cnn_model.add(Convolution2D(64, 3, 3, trainable=trainable, activation=args.activation))
+#     # cnn_model.add(Flatten())  # 3136
+#     # cnn_model.add(Dense(512, trainable=trainable))
+#     # cnn_model.add(Activation(args.activation))
+#     cnn_model.add(Flatten())
+#
+#     cnn_model.add(Dense(env.action_space.n + 1, activation='softmax'))
+#
+#     # init_norm = Gaussian(loc=0.0, scale=0.01)
+#     # layers = []
+#     # layers.append(Input(shape=env.observation_space.shape))
+#     # # The first hidden layer convolves 32 filters of 8x8 with stride 4 with the input image and applies a rectifier nonlinearity.
+#     # layers.append(Conv((8, 8, 32), strides=4, init=init_norm, activation=args.activation)(h), batch_norm=args.batch_norm)
+#     # # The second hidden layer convolves 64 filters of 4x4 with stride 2, again followed by a rectifier nonlinearity.
+#     # layers.append(Conv((4, 4, 64), strides=2, init=init_norm, activation=args.activation)(h), batch_norm=args.batch_norm)
+#     # # This is followed by a third convolutional layer that convolves 64 filters of 3x3 with stride 1 followed by a rectifier.
+#     # layers.append(Conv((3, 3, 64), strides=1, init=init_norm, activation=args.activation)(h), batch_norm=args.batch_norm)
+#     # # The final hidden layer is fully-connected and consists of 512 rectifier units.
+#     # layers.append(Affine(nout=512, init=init_norm, activation=args.activation)(h), batch_norm=self.batch_norm)
+#     # # The output layer is a fully-connected linear layer with a single output for each valid action.
+#     # # layers.append(Affine(nout=num_actions, init = init_norm))
+#
+#     # y = Dense(env.action_space.n + 1)(h)
+#     # if args.advantage == 'avg':
+#     #     z = Lambda(lambda a: K.expand_dims(a[:, 0], dim=-1) + a[:, 1:] - K.mean(a[:, 1:], keepdims=True),
+#     #                output_shape=(env.action_space.n,))(y)
+#     # elif args.advantage == 'max':
+#     #     z = Lambda(lambda a: K.expand_dims(a[:, 0], dim=-1) + a[:, 1:] - K.max(a[:, 1:], keepdims=True),
+#     #                output_shape=(env.action_space.n,))(y)
+#     # elif args.advantage == 'naive':
+#     #     z = Lambda(lambda a: K.expand_dims(a[:, 0], dim=-1) + a[:, 1:], output_shape=(env.action_space.n,))(y)
+#     # else:
+#     #     assert False
+#     # cnn_model.add(z)
+#     return cnn_model
+
+# def old_createLayers():
+#     x = Input(shape=env.observation_space.shape)
+#     if args.batch_norm:
+#         h = BatchNormalization()(x)
+#     else:
+#         h = x
+#     for i in range(args.layers):
+#         h = Dense(args.hidden_size, activation=args.activation)(h)
+#         if args.batch_norm and i != args.layers - 1:
+#             h = BatchNormalization()(h)
+#     y = Dense(env.action_space.n + 1)(h)
+#     if args.advantage == 'avg':
+#         z = Lambda(lambda a: K.expand_dims(a[:, 0], dim=-1) + a[:, 1:] - K.mean(a[:, 1:], keepdims=True),
+#                    output_shape=(env.action_space.n,))(y)
+#     elif args.advantage == 'max':
+#         z = Lambda(lambda a: K.expand_dims(a[:, 0], dim=-1) + a[:, 1:] - K.max(a[:, 1:], keepdims=True),
+#                    output_shape=(env.action_space.n,))(y)
+#     elif args.advantage == 'naive':
+#         z = Lambda(lambda a: K.expand_dims(a[:, 0], dim=-1) + a[:, 1:], output_shape=(env.action_space.n,))(y)
+#     else:
+#         assert False
+#
+#     return x, z
 
 
 parser = argparse.ArgumentParser()
@@ -49,7 +161,8 @@ parser.add_argument('--learning_rate', type=float, default=0.00025)
 parser.add_argument('--episodes', type=int, default=1000)
 parser.add_argument('--max_timesteps', type=int, default=200)
 parser.add_argument('--activation', choices=['tanh', 'relu'], default='tanh')
-parser.add_argument('--optimizer', choices=['adam', 'rmsprop'], default='adam')
+# parser.add_argument('--optimizer', choices=['adam', 'rmsprop'], default='adam')
+parser.add_argument('--optimizer', choices=['adam', 'rmsprop'], default='rmsprop')
 parser.add_argument('--ep_start', type=float, default=1)
 parser.add_argument('--ep_end', type=float, default=0.1)
 parser.add_argument('--ep_endt', type=float, default=1000000)
@@ -58,8 +171,8 @@ parser.add_argument('--display', action='store_true', default=True)
 parser.add_argument('--no_display', dest='display', action='store_false')
 parser.add_argument('--gym_record')
 parser.add_argument('--update_frequency', type=int, default=4)
-parser.add_argument('--target_net_update_frequency', type=int, default=32)
-parser.add_argument('--replay_memory_size', type=int, default=1000000)
+parser.add_argument('--target_net_update_frequency', type=int, default=5000) #10000
+parser.add_argument('--replay_memory_size', type=int, default=50000) #1000000
 
 parser.add_argument('environment')
 args = parser.parse_args()
@@ -71,15 +184,17 @@ assert isinstance(env.action_space, Discrete)
 if args.gym_record:
     env.monitor.start(args.gym_record, force=True)
 
-x, z = createLayers()
-model = Model(input=x, output=z)
+model = create_model()
 model.summary()
-optimizer = Adam(lr=args.learning_rate)
-# optimizer = RMSprop(lr=args.learning_rate)
+if (args.optimizer == "adam"):
+    optimizer = Adam(lr=args.learning_rate)
+elif (args.optimizer == "rmsprop"):
+    optimizer = RMSprop(lr=args.learning_rate)
+else:
+    assert False, "invalid optimizer"
 model.compile(optimizer=optimizer, loss='mse')
 
-x, z = createLayers()
-target_model = Model(input=x, output=z)
+target_model = create_model()
 target_model.set_weights(model.get_weights())
 
 prestates = []
@@ -94,8 +209,13 @@ train_steps = 0
 
 for i_episode in range(args.episodes):
     observation = env.reset()
+    observation_t1 = observation
     episode_reward = 0
-    for t in range(args.max_timesteps):
+
+    # for t in range(args.max_timesteps):
+    t = -1
+    while True:
+        t += 1
         if args.display:
             env.render()
 
@@ -120,6 +240,8 @@ for i_episode in range(args.episodes):
             del poststates[delidx]
             del terminals[delidx]
 
+        # prestates.append(observation.ravel())
+        # prestates.append(observation.reshape([1] + list(env.observation_space.shape)))
         prestates.append(observation)
         actions.append(action)
 
@@ -129,8 +251,12 @@ for i_episode in range(args.episodes):
             print("reward:", reward)
 
         rewards.append(reward)
+        # poststates.append(observation.ravel())
+        # poststates.append(observation.reshape([1] + list(env.observation_space.shape)))
         poststates.append(observation)
         terminals.append(done)
+
+        observation_t1 = observation
 
         timestep += 1
 
@@ -139,14 +265,16 @@ for i_episode in range(args.episodes):
             if timestep % args.update_frequency == 0:
                 for k in xrange(args.train_repeat):
                     if len(prestates) > args.batch_size:
-                        # indexes = range(args.batch_size)
-                        # indexes = np.random.choice(len(prestates), size=args.batch_size)
                         indexes = np.random.randint(len(prestates), size=args.batch_size)
                     else:
                         indexes = range(len(prestates))
 
                     pre_sample = np.array([prestates[i] for i in indexes])
                     post_sample = np.array([poststates[i] for i in indexes])
+
+                    # a = pre_sample[0]
+                    # qpre = model.predict(a)
+
                     qpre = model.predict(pre_sample)
                     qpost = target_model.predict(post_sample)
                     for i in xrange(len(indexes)):
