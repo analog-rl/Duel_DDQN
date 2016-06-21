cc8fdb52366ffbd3930325582280c7ec27255c9b
Joe Booth <joe@joebooth.com>
2016-06-21 06:56:00 -0700

    [wip] first plot

--- duel.py
+++ duel.py
@@ -9,6 +9,11 @@ from keras.optimizers import Adam, RMSprop
 from keras import initializations
 import numpy as np
 
+import matplotlib
+# matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from statbin import statbin
+
 
 def createLayers():
     # def custom_init(shape, scale=0.01, name=None):
@@ -39,6 +44,47 @@ def createLayers():
     return x, z
 
 
+def plot_stats(episode):
+    # if self.ipy_clear:
+    #     from IPython import display
+    #     display.clear_output(wait=True)
+    fig = plt.figure(1)
+    fig.canvas.set_window_title("Training Stats for %s" % (episode))
+    plt.clf()
+    plt.subplot(2, 2, 1)
+    stats["tr"].plot()
+    plt.title("Total Reward per Episode")
+    plt.xlabel("Episode")
+    plt.ylabel("Total Reward")
+    plt.legend(loc=2)
+    plt.subplot(2, 2, 2)
+    stats["ft"].plot()
+    plt.title("Finishing Time per Episode")
+    plt.xlabel("Episode")
+    plt.ylabel("Finishing Time")
+    plt.legend(loc=2)
+    plt.subplot(2, 2, 3)
+    stats["maxvf"].plot2(fill_col='lightblue', label='Avg Max VF')
+    stats["minvf"].plot2(fill_col='slategrey', label='Avg Min VF')
+    plt.title("Value Function Outputs")
+    plt.xlabel("Episode")
+    plt.ylabel("Value Fn")
+    plt.legend(loc=2)
+    ax = plt.subplot(2, 2, 4)
+    stats["cost"].plot2()
+    plt.title("Training Loss")
+    plt.xlabel("Training Epoch")
+    plt.ylabel("Loss")
+    try:
+        #                ax.set_yscale("log", nonposy='clip')
+        plt.tight_layout()
+    except:
+        pass
+    plt.show(block=False)
+    plt.draw()
+    plt.pause(0.001)
+
+debug_scale = 1.0
 parser = argparse.ArgumentParser()
 parser.add_argument('--verbose', type=int, default=0)
 parser.add_argument('--batch_size', type=int, default=32)
@@ -46,15 +92,17 @@ parser.add_argument('--hidden_size', type=int, default=100)
 parser.add_argument('--layers', type=int, default=1)
 parser.add_argument('--batch_norm', action="store_true", default=False)
 parser.add_argument('--no_batch_norm', action="store_false", dest='batch_norm')
-parser.add_argument('--replay_start_size', type=int, default=50000)
+parser.add_argument('--replay_start_size', type=int, default=50000 * debug_scale)
 parser.add_argument('--train_repeat', type=int, default=10)
 parser.add_argument('--gamma', type=float, default=0.99)
 parser.add_argument('--tau', type=float, default=0.001)
 parser.add_argument('--learning_rate', type=float, default=0.00025)
 parser.add_argument('--episodes', type=int, default=1000)
 parser.add_argument('--max_timesteps', type=int, default=200)
-parser.add_argument('--activation', choices=['tanh', 'relu'], default='tanh')
-parser.add_argument('--optimizer', choices=['adam', 'rmsprop'], default='adam')
+# parser.add_argument('--activation', choices=['tanh', 'relu'], default='tanh')
+# parser.add_argument('--optimizer', choices=['adam', 'rmsprop'], default='adam')
+parser.add_argument('--activation', choices=['tanh', 'relu'], default='relu')
+parser.add_argument('--optimizer', choices=['adam', 'rmsprop'], default='rmsprop')
 parser.add_argument('--ep_start', type=float, default=1)
 parser.add_argument('--ep_end', type=float, default=0.1)
 parser.add_argument('--ep_endt', type=float, default=1000000)
@@ -63,8 +111,11 @@ parser.add_argument('--display', action='store_true', default=True)
 parser.add_argument('--no_display', dest='display', action='store_false')
 parser.add_argument('--gym_record')
 parser.add_argument('--update_frequency', type=int, default=4)
+# parser.add_argument('--target_net_update_frequency', type=int, default=10000 * debug_scale)
 parser.add_argument('--target_net_update_frequency', type=int, default=32)
-parser.add_argument('--replay_memory_size', type=int, default=1000000)
+parser.add_argument('--replay_memory_size', type=int, default=1000000 * debug_scale)
+parser.add_argument('--plot_frequency', type=int, default=10)
+
 
 parser.add_argument('environment')
 args = parser.parse_args()
@@ -97,22 +148,36 @@ total_reward = 0
 timestep = 0
 train_steps = 0
 
+stats = None
+# if self.enable_plots:
+if stats is None:
+    stats = {
+        "tr": statbin(args.plot_frequency),  # Total Reward
+        "ft": statbin(args.plot_frequency),  # Finishing Time
+        "minvf": statbin(args.plot_frequency),  # Min Value Fn
+        "maxvf": statbin(args.plot_frequency),  # Min Value Fn
+        "cost": statbin(args.plot_frequency),  # Loss
+    }
 for i_episode in range(args.episodes):
     observation = env.reset()
     episode_reward = 0
+    episode_ave_max_q = 0
+    episode_ave_min_q = 0
+    episode_ave_cost = []
+    ep_t = 0
     for t in range(args.max_timesteps):
         if args.display:
             env.render()
 
         ep = args.ep_end + (args.ep_start - args.ep_end) * max(0, args.ep_endt - train_steps) / args.ep_endt
 
+        s = np.array([observation])
+        q = model.predict(s, batch_size=1)
         if timestep < args.replay_start_size or np.random.random() < ep:
             action = env.action_space.sample()
             if args.verbose > 0:
                 print "e:", i_episode, "e.t:", t, "action:", action, "random", " ep:", ep
         else:
-            s = np.array([observation])
-            q = model.predict(s, batch_size=1)
             action = np.argmax(q[0])
             if args.verbose > 0:
                 print "e:", i_episode, "e.t:", t, "action:", action, "q:", q, " ep:", ep
@@ -129,7 +194,6 @@ for i_episode in range(args.episodes):
         actions.append(action)
 
         observation, reward, done, info = env.step(action)
-        episode_reward += reward
         if args.verbose > 1:
             print("reward:", reward)
 
@@ -138,6 +202,12 @@ for i_episode in range(args.episodes):
         terminals.append(done)
 
         timestep += 1
+
+        episode_reward += reward
+        episode_ave_max_q += np.max(q)
+        episode_ave_min_q += np.min(q)
+        ep_t += 1
+
 
         if timestep > args.replay_start_size:
             train_steps += 1
@@ -152,14 +222,26 @@ for i_episode in range(args.episodes):
 
                     pre_sample = np.array([prestates[i] for i in indexes])
                     post_sample = np.array([poststates[i] for i in indexes])
-                    qpre = model.predict(pre_sample)
+                    qpre = model.predict(pre_sample).copy()
                     qpost = target_model.predict(post_sample)
                     for i in xrange(len(indexes)):
                         if terminals[indexes[i]]:
                             qpre[i, actions[indexes[i]]] = rewards[indexes[i]]
                         else:
                             qpre[i, actions[indexes[i]]] = rewards[indexes[i]] + args.gamma * np.amax(qpost[i])
-                    model.train_on_batch(pre_sample, qpre)
+                    loss = model.train_on_batch(pre_sample, qpre)
+
+                    # # Define cost and gradient update op
+                    # a = tf.placeholder("float", [None, self.env[0].action_space.n])
+                    # y = tf.placeholder("float", [None])
+                    # action_q_values = tf.reduce_sum(tf.mul(q_values, a), reduction_indices=1)
+                    # cost = tf.reduce_mean(tf.square(y - action_q_values))
+                    # optimizer = tf.train.AdamOptimizer(self.learning_rate)
+                    # grad_update = optimizer.minimize(cost, var_list=network_params)
+                    # cost = ops["cost"].eval(session=self.parent.session, feed_dict=fd)
+                    cost = loss
+                    episode_ave_cost.append(cost)
+
 
             if timestep % args.target_net_update_frequency == 0:
                 if args.verbose > 0:
@@ -177,10 +259,25 @@ for i_episode in range(args.episodes):
         if done:
             break
 
+    stats['tr'].add(episode_reward)
+    stats['ft'].add(ep_t)
+    stats['maxvf'].add(episode_ave_max_q / float(ep_t))
+    stats['minvf'].add(episode_ave_min_q / float(ep_t))
+    stats['cost'].add(np.mean(episode_ave_cost))
+        # 'maxvf': episode_ave_max_q / float(ep_t),
+        # 'minvf': episode_ave_min_q / float(ep_t),
+        # 'cost': np.mean(episode_ave_cost)
+
+
     print("Episode {} finished after {} timesteps, episode reward {}".format(i_episode + 1, t + 1, episode_reward))
     total_reward += episode_reward
+
+    if (i_episode % args.plot_frequency == 0):
+        plot_stats(i_episode)
 
 print("Average reward per episode {}".format(total_reward / args.episodes))
 
 if args.gym_record:
     env.monitor.close()
+
+
